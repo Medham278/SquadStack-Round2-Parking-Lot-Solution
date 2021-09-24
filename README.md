a8890bdde73aaa2da8c531405414259c5ba6013e
Medha Mathur <58322032+Medham278@users.noreply.github.com>
2021-09-24 18:49:56 +0000

    Added README.md and comments in main.py

---
+++ README.md
@@ -0,0 +1,33 @@
+# Problem Statement :
+We own a parking lot that can hold up to ‘n’ cars at any given point in time. Each slot is given a
+number starting at one increasing with increasing distance from the entry point in steps of one.
+We want to create an automated ticketing system that allows our customers to use our parking
+lot without human intervention.
+When a car enters the parking lot, we want to have a ticket issued to the driver. The ticket
+issuing process includes:-
+
+1. We are taking note of the number written on the vehicle registration plate and the age of
+the driver of the car.
+2. And we are allocating an available parking slot to the car before actually handing over a
+ticket to the driver (we assume that our customers are kind enough to always park in the
+slots allocated to them).
+The customer should be allocated a parking slot that is nearest to the entry. At the exit, the
+customer returns the ticket, marking the slot they were using as being available.
+Due to government regulation, the system should provide us with the ability to find out:-<br>
+● Vehicle Registration numbers for all cars which are parked by the driver of a certain age,<br>
+● Slot number in which a car with a given vehicle registration plate is parked.<br>
+● Slot numbers of all slots where cars of drivers of a particular age are parked.<br>
+We get the input by reading input.txt directly (you’ll have to create it in your environment) .The
+file will contain a set of commands separated by a newline, we need to execute the commands
+in order and produce output.
+
+# Approach :
+A utility class named "Parking" is created with the following functions-
+1. init() - Initializes a Parkig Lot of specific size <br>
+2. park() - Parks the car into the nearest available Parking space<br>
+3. getSlotAge() - Gets all the slot numbers (comma-separated) where age of driver is equal to the given age.<br>
+4. getSlotRno() - Gets all the slot numbers (comma-separated) where Registration Number of driver is equal to the given Registration Number.<br>
+5. getRno() - Gets all the Registration Numbers (comma-separated) where age of driver is equal to the given age.<br>
+6. leave() - will vacate the specified Parking slot.<br>
+
+Necessary statements have been added in cases where the command cannot be performed. This makes the program understandable during runtime.
--- main.py
+++ main.py
@@ -5,59 +5,68 @@ Created on Fri Sep 24 22:36:17 2021
 @author: MEDHA MATHUR
 """
 class Parking:
+    #Create Parking lot of given size(val)
     def __init__(self,val=0):
         self.numLots=val
-        self.lots=[0]*self.numLots
+        self.lots=[0]*self.numLots #Initialize to 0
     
+    #Park car into nearest available lot by traversing from 1st to last lot
     def park(self,rno,age):
         for l in range(len(self.lots)):
             if self.lots[l]==0:
                 self.lots[l]=[rno,age]
                 return l+1
-        return -1           
+        return -1 #If no slot is available          
     
+    #Get all slot numbers corresponding to given age
     def getSlotAge(self,age):
         ans=""
         for i in range(len(self.lots)):
-            if self.lots[i]==0: continue
+            if self.lots[i]==0: continue #No car parked in this lot
             elif self.lots[i][1]==age:
                 ans+=','+str(i+1) if len(ans)>0 else str(i+1)
-        return ans
+        return ans #comma separated slots outputed
     
+    #Get all slot numbers corresponding to given Registration Number
     def getSlotRno(self,rno):
         ans=""
         for i in range(len(self.lots)):
-            if self.lots[i]==0: continue
+            if self.lots[i]==0: continue #No car parked in this lot
             elif self.lots[i][0]==rno:
                 ans+=','+str(i+1) if len(ans)>0 else str(i+1)
-        return ans
+        return ans #comma separated slots outputed
     
+    #Get all Registration Numbers corresponding to given age
     def getRno(self,age):
         ans=""
         for i in range(len(self.lots)):
-            if self.lots[i]==0: continue
+            if self.lots[i]==0: continue #No car parked in this lot
             elif self.lots[i][1]==age:
                 ans+=','+self.lots[i][0] if len(ans)>0 else self.lots[i][0]
-        return ans
+        return ans #comma separated Registration Numbers outputed
     
+    #Vacate the given slot
     def leave(self,slot):
+        # If input slot number is more than size of parking lot
         if slot>len(self.lots):
             return 2
         elif self.lots[slot-1]==0:
-            return 1
+            return 1 #The input slot is already empty
         else:
             rno,age=self.lots[slot-1][0],self.lots[slot-1][1]
             self.lots[slot-1]=0
-            return [rno,age]
+            return [rno,age] # Returning details of driver who vacated slot
 
 if __name__=="__main__":
-    obj=Parking()
-    file1 = open('input.txt', 'r')
-    flines = file1.readlines()
+    obj=Parking() #Create an empty object
+    #opening input and output files 
+    file1 = open('input.txt', 'r') 
+    flines = file1.readlines() 
     file2 = open('output.txt', 'x')
     for line in flines:
         line=line.strip()
-        if line=='': break
+        if line=='': break #If EOF is reached, we stop processing
+        #Splitting the command name from the inputs
         cmd = list(map(str,line.split()))
         if cmd[0]=="Create_parking_lot":
             obj=Parking(int(cmd[1]))
@@ -86,9 +95,10 @@ if __name__=="__main__":
             ans=obj.getRno(cmd[1])
             file2.write(ans+'\n')
         else:
-            file2.write("Invalid Command\n")
+            file2.write("Invalid Command\n") #If any other command is entered, then it is INVALID
     file1.close()
     file2.close()
+    #Removing the last line from output file which is an extra '\n'
     lines = open('output.txt', 'r').readlines() 
     del lines[-1] 
     open('output.txt', 'w').writelines(lines) 
